fe08014a51b1271c683b37cb5b061686dd9545a5
MartimJales <m.jales@campus.fct.unl.pt>
2026-06-04 19:37:57 +0100

    Delete sandbox dir

--- sandbox/run_padtai_sandbox.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Run PADTAI sandbox runner for clusters 0 and 1 with 10min timeout
-
-# LT-only grounded operator
-GROUNDED=${GROUNDED:-"lt:LTOperator"}
-
-python3 sandbox/run_padtai_sandbox.py --clusters 0 1 --timeout 600 --intcols auto --grounded "$GROUNDED"
